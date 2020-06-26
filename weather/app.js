366ff9db79c5cdfed2de3e1f5d96a92f4be63caf
efraesco <efraesco@gmail.com>
2020-06-26 01:52:16 -0500

    add comments to app.js

--- weather/app.js
+++ weather/app.js
@@ -18,7 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Express.js routine called indexRouter
+app.use('/', indexRouter);  
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
